03c97d17621493edc457dd06e51333bdcc5fb471
Fatih Durmaz <71706970+fatihdurmaz@users.noreply.github.com>
2023-11-20 13:11:02 +0300

    Initial Commit

---
+++ SDWebImageExample/SDWebImageExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDWebImageExampleApp.swift
+//  SDWebImageExample
+//
+//  Created by Fatih Durmaz on 20.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SDWebImageExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
